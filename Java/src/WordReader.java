e9cfcee8d202748a790d5b4fb50d7f026c0958b5
Balraj Singh Bains <polltery@gmail.com>
2022-01-10 23:27:17 +0800

    initial commit

---
+++ Java/src/WordReader.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class WordReader {
+    public static void main(String[] args) throws FileNotFoundException {
+        String localDir = System.getProperty("user.dir");
+        System.out.println(localDir);
+        String path = localDir + File.separator + "words.txt";
+
+        InputStream is = new FileInputStream(path);
+
+        try (Scanner sc = new Scanner(
+                is, StandardCharsets.UTF_8.name())) {
+            System.out.println("words=[");
+            while (sc.hasNextLine()) {
+                String text = sc.nextLine();
+                if(text.matches("^[a-zA-Z]{5}$")){
+                    System.out.println(text + ",");
+                }
+            }
+            System.out.println("]");
+        }
+    }
+}
---
+++ app.js
@@ -0,0 +1,67 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        possibleWords: words,
+        query: "?????",
+        excludeLetters: "",
+        result: "Results will appear here",
+        displayedMatches: []
+    },
+    methods:{
+        search: function(event){
+            this.result = "Validating...";
+            if(this.validateQuery()){
+                this.result = "Searching...";
+                var matches = []
+                var queryRegex = this.buildQueryRegex();
+                for(i = 0; i < this.possibleWords.length; i++){
+                    var possibleWord = this.possibleWords[i];
+                    if(queryRegex.test(possibleWord.toLowerCase())){
+                        matches.push(possibleWord);
+                        if(this.displayedMatches.length < 100){
+                            this.displayedMatches.push(possibleWord.toUpperCase())
+                        }
+                    }
+                }
+                if(matches.length > 0){
+                    this.result = "" + matches.length + " words matched.";
+                }else{
+                    this.result = "No match.";
+                }
+            }else{
+                this.result = "Invalid search query.";
+            }
+        },
+        validateQuery: function(){
+            var validationRegex = new RegExp("^[a-z\?\*]{5,10}$");
+            var validationRegexForExcludedLetters = new RegExp("^[a-z]{0,19}$");
+            this.displayedMatches = []
+            if(validationRegex.test(this.query.toLowerCase())
+                && validationRegexForExcludedLetters.test(this.excludeLetters.toLowerCase())){
+               return true;
+            }
+            return false;
+        },
+        buildQueryRegex: function(){
+            var queryRegex = "";
+            var includeLetters = "";
+            for (var i = 0; i < this.query.length; i++) {
+                var letter = this.query[i];
+                if(letter === "?"){
+                    queryRegex += ".";
+                }else{
+                    if(i+1 <= this.query.length && this.query[i+1] === "*"){
+                        includeLetters += letter.toLowerCase();
+                        queryRegex += ".";
+                        i += 1;
+                    }else{
+                        queryRegex += letter.toLowerCase();
+                    }
+                }
+            }
+            var excludeRegex = this.excludeLetters.length > 0 ? "(?!.*[" + this.excludeLetters.toLowerCase() + "])" : "";     
+            var includeRegex = includeLetters.length > 0 ? "(?=.*[" + includeLetters.toLowerCase() + "])" : "";
+            return new RegExp("^" + excludeRegex + includeRegex + queryRegex + "$");
+        }
+    }
+})
